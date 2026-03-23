40d9ec5ef4786b3957847ab527c85c2d41079cb7
Oscar Matos <omatosperez851@gmail.com>
2026-03-23 01:28:38 -0400

    refactor: remove LayoutGroup from layout-provider

--- src/components/providers/layout-provider.tsx
+++ src/components/providers/layout-provider.tsx
@@ -1,6 +1,3 @@
-"use client";
-import { LayoutGroup } from "motion/react";
-
 export function LayoutProvider({ children }: { children: React.ReactNode }) {
-  return <LayoutGroup>{children}</LayoutGroup>;
+  return <>{children}</>;
 }
